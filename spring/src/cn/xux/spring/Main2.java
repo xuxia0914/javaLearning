e6adbb2404b004ac1866b7d1862cc523ad02b7d2
xuxia0914 <315307897@qq.com>
2019-08-26 22:24:48 +0800

    2019-08-26-spring-自动装配-bean的继承和依赖

--- spring/src/cn/xux/spring/Main2.java
+++ spring/src/cn/xux/spring/Main2.java
@@ -15,22 +15,34 @@ public class Main2 {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         Person person3 = (Person)ctx.getBean("person3");
-        System.out.println(person3);
+        System.out.println("person3 = " + person3);
 
         NewPerson person4 = (NewPerson)ctx.getBean("person4");
-        System.out.println(person4);
+        System.out.println("person4 = " + person4);
 
-        DataSource ds = (DataSource)ctx.getBean("dataSource");
-        System.out.println(ds);
+        DataSource dataSource = (DataSource)ctx.getBean("dataSource");
+        System.out.println("dataSource = " + dataSource);
 
-        List<Car> list = (List)ctx.getBean("cars");
-        System.out.println(list);
+        List<Car> carList = (List)ctx.getBean("cars");
+        System.out.println("carList = " + carList);
 
         Person person5 = (Person)ctx.getBean("person5");
-        System.out.println(person5);
+        System.out.println("person5 = " + person5);
 
         Person person6 = (Person)ctx.getBean("person6");
-        System.out.println(person6);
+        System.out.println("person6 = " + person6);
+
+        Person person7 = (Person)ctx.getBean("person7");
+        System.out.println("person7 = " + person7);
+
+        /*Person person8 = (Person)ctx.getBean("person8");
+        System.out.println(person8);*/
+
+        Person person9 = (Person)ctx.getBean("person9");
+        System.out.println("person9 = " + person9);
+
+        Person person10 = (Person)ctx.getBean("person10");
+        System.out.println("person10 = " + person10);
 
     }
 
