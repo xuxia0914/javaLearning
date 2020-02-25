2b371d222153527323b1c7f6f786308d249bc468
xuxia0914 <315307897@qq.com>
2020-02-25 22:23:28 +0800

    2020-02-25-Manacher算法求最长回文子串

--- leetcode/src/cn/leetcode/xux/midium/LongestPalindromicSubString.java
+++ leetcode/src/cn/leetcode/xux/midium/LongestPalindromicSubString.java
@@ -13,7 +13,56 @@ package cn.leetcode.xux.midium;
 
 public class LongestPalindromicSubString {
 
+    /**Manacher算法*/
     public String longestPalindrome(String s) {
+        if(s==null||s.length()<2) {
+            return s;
+        }
+        //预处理字符串
+        StringBuilder sb = new StringBuilder('#');
+        for(char c : s.toCharArray()) {
+            sb.append(c).append('#');
+        }
+        String str = sb.toString();
+        int len = str.length();
+        //以i为中心的回文串的半径
+        int[] dp = new int[len];
+        //当前所有回文能到达的最大右边界
+        int max = 0;
+        //当前所有回文能到达最大右边界的回文的中心
+        int id = 0;
+        //最大回文的中心和半径
+        int[] res = new int[2];
+        for(int i=0;i<len;i++) {
+            //manacher算法思想的关键
+            if(i<max) {
+                dp[i] = Math.min(dp[2*id-i], max-i);
+            }else {
+                dp[i] = 1;
+            }
+            //暴力扩展
+            while(i-dp[i]>=0&&i+dp[i]<len&&str.charAt(i-dp[i])==str.charAt(i+dp[i])) {
+                dp[i]++;
+            }
+            if(i+dp[i]>max) {
+                max = i+dp[i];
+                id = i;
+            }
+            if(dp[i]>res[1]) {
+                res[0] = i;
+                res[1] = dp[i];
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for(int i=res[0]-res[1]+1;i<res[0]+res[1];i++) {
+            if(str.charAt(i)!='#') {
+                result.append(str.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+
+    public String longestPalindrome1(String s) {
         if(s==null||s.length()<2) {
             return s;
         }
