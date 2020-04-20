2205e5c6a80db5254091f87475a13f526c092a7a
徐侠 <xux21@chinaunicom.cn>
2020-04-20 17:21:34 +0800

    2020-04-20

--- leetcode/src/cn/leetcode/xux/midium/NumberOfMatchingSubsequences.java
+++ leetcode/src/cn/leetcode/xux/midium/NumberOfMatchingSubsequences.java
@@ -1,5 +1,8 @@
 package cn.leetcode.xux.midium;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 792. 匹配子序列的单词数
  * 给定字符串 S 和单词字典 words, 求 words[i] 中是 S 的子序列的单词个数。
@@ -19,25 +22,39 @@ package cn.leetcode.xux.midium;
  */
 public class NumberOfMatchingSubsequences {
 
-    //TODO
     public int numMatchingSubseq(String S, String[] words) {
         if(S==null||S.length()==0||words==null||words.length==0) {
             return 0;
         }
-        int n = words.length;
-        int len = S.length();
-        int[] idxs = new int[n];
-        for(int i=0;i<len;i++) {
-            for(int j=0;j<n;j++) {
-                if(idxs[j]<words[j].length()&&S.charAt(i)==words[j].charAt(idxs[j])) {
-                    idxs[j]++;
+        Queue<String>[] mem = new Queue[26];
+        for(String word : words) {
+            if(word!=null||word.length()!=0) {
+                int idx = word.charAt(0)-'a';
+                if(mem[idx]==null) {
+                    mem[idx] = new LinkedList<>();
                 }
+                mem[idx].offer(word);
             }
         }
+        int len = S.length();
         int result = 0;
-        for(int i=0;i<n;i++) {
-            if(idxs[i]==words[i].length()) {
-                result++;
+        for(int i=0;i<len;i++) {
+            int idx = S.charAt(i)-'a';
+            if(mem[idx]!=null) {
+                int size = mem[idx].size();
+                while(size-->0) {
+                    String curr = mem[idx].poll();
+                    if(curr.length()==1) {
+                        result++;
+                    }else {
+                        String tmp = curr.substring(1);
+                        int nextIdx = tmp.charAt(0)-'a';
+                        if(mem[nextIdx]==null) {
+                            mem[nextIdx] = new LinkedList<>();
+                        }
+                        mem[nextIdx].offer(tmp);
+                    }
+                }
             }
         }
         return result;
