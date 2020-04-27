f1dd0e160beca5f025fe5e91638231f0cb0a4c2a
徐侠 <xux21@chinaunicom.cn>
2020-04-27 17:24:03 +0800

    2020-04-27

--- leetcode/src/cn/leetcode/xux/midium/ConstructBinaryTree.java
+++ leetcode/src/cn/leetcode/xux/midium/ConstructBinaryTree.java
@@ -90,4 +90,47 @@ public class ConstructBinaryTree {
         return node;
     }
 
+    /**
+     * 889. 根据前序和后序遍历构造二叉树
+     * 返回与给定的前序和后序遍历匹配的任何二叉树。
+     *  pre 和 post 遍历中的值是不同的正整数。
+     *
+     * 示例：
+     * 输入：pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
+     * 输出：[1,2,3,4,5,6,7]
+     *
+     * 提示：
+     * 1 <= pre.length == post.length <= 30
+     * pre[] 和 post[] 都是 1, 2, ..., pre.length 的排列
+     * 每个输入保证至少有一个答案。如果有多个答案，可以返回其中一个。
+     */
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        if(pre==null||pre.length==0) {
+            return null;
+        }
+        int len = pre.length;
+        return helper3(pre, 0, len-1, post, 0, len-1);
+    }
+
+    public TreeNode helper3(int[] pre, int preStart, int preEnd,
+            int[] post, int postStart, int postEnd) {
+        if(preStart>preEnd) {
+            return null;
+        }
+        TreeNode result = new TreeNode(pre[preStart]);
+        if(preStart+1<=preEnd) {
+            int i;
+            for(i=postStart;i<=preEnd;i++) {
+                if(post[i]==pre[preStart+1]) {
+                    break;
+                }
+            }
+            result.left = helper3(pre, preStart+1, i-postStart+preStart+1,
+                    post, postStart, i);
+            result.right = helper3(pre, i-postStart+preStart+2, preEnd,
+                    post, i+1, postEnd-1);
+        }
+        return result;
+    }
+
 }
