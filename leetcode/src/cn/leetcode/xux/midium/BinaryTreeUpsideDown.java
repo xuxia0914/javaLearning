c263c16988f5bfa2ed738ddb9e71004b0dd244bf
徐侠 <xux21@chinaunicom.cn>
2020-05-12 17:32:44 +0800

    2020-05-12

--- leetcode/src/cn/leetcode/xux/midium/BinaryTreeUpsideDown.java
+++ leetcode/src/cn/leetcode/xux/midium/BinaryTreeUpsideDown.java
@@ -3,7 +3,7 @@ package cn.leetcode.xux.midium;
 import cn.leetcode.xux.common.TreeNode;
 
 /**
- * Given a binary tree where all the right nodes are either leaf nodes
+ * 156 Given a binary tree where all the right nodes are either leaf nodes
  * with a sibling (a left node that shares the same parent node) or empty,
  * flip it upside down and turn it into a tree where the original right nodes
  * turned into left leaf nodes. Return the new root.
