ed80b14f5f3d1053b775d01ee9ce1a41c89eec82
徐侠 <xux21@chinaunicom.cn>
2020-05-27 17:15:41 +0800

    2020-05-27

---
+++ leetcode/src/cn/leetcode/xux/easy/CountNegativeNumbersInASortedMatrix.java
@@ -0,0 +1,50 @@
+package cn.leetcode.xux.easy;
+
+/**
+ * 1351. 统计有序矩阵中的负数
+ * 给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以非递增顺序排列。
+ * 请你统计并返回 grid 中 负数 的数目。
+ *
+ * 示例 1：
+ * 输入：grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+ * 输出：8
+ * 解释：矩阵中共有 8 个负数。
+ *
+ * 示例 2：
+ * 输入：grid = [[3,2],[1,0]]
+ * 输出：0
+ *
+ * 示例 3：
+ * 输入：grid = [[1,-1],[-1,-1]]
+ * 输出：3
+ *
+ * 示例 4：
+ * 输入：grid = [[-1]]
+ * 输出：1
+ *
+ * 提示：
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 100
+ * -100 <= grid[i][j] <= 100
+ */
+public class CountNegativeNumbersInASortedMatrix {
+
+    public int countNegatives(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int i = 0;
+        int j = n-1;
+        int ans = 0;
+        while(i<m&&j>=0) {
+            if(grid[i][j]<0) {
+                ans += m-i;
+                j--;
+            }else {
+                i++;
+            }
+        }
+        return ans;
+    }
+
+}
---
+++ leetcode/src/cn/leetcode/xux/midium/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -0,0 +1,53 @@
+package cn.leetcode.xux.midium;
+
+/**
+ * 1358. 包含所有三种字符的子字符串数目
+ * 给你一个字符串 s ，它只包含三种字符 a, b 和 c 。
+ * 请你返回 a，b 和 c 都 至少 出现过一次的子字符串数目。
+ *
+ * 示例 1：
+ * 输入：s = "abcabc"
+ * 输出：10
+ * 解释：包含 a，b 和 c 各至少一次的子字符串为 "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" 和 "abc" (相同字符串算多次)。
+ *
+ * 示例 2：
+ * 输入：s = "aaacb"
+ * 输出：3
+ * 解释：包含 a，b 和 c 各至少一次的子字符串为 "aaacb", "aacb" 和 "acb" 。
+ *
+ * 示例 3：
+ * 输入：s = "abc"
+ * 输出：1
+ *
+ * 提示：
+ * 3 <= s.length <= 5 x 10^4
+ * s 只包含字符 a，b 和 c 。
+ */
+public class NumberOfSubstringsContainingAllThreeCharacters {
+
+    public int numberOfSubstrings(String s) {
+        int lastA = -1;
+        int lastB = -1;
+        int lastC = -1;
+        int len = s.length();
+        int ans = 0;
+        for(int i=0;i<len;i++) {
+            char c = s.charAt(i);
+            if(c=='a') {
+                int left = Math.min(lastB, lastC);
+                ans += left+1;
+                lastA = i;
+            }else if(c=='b') {
+                int left = Math.min(lastA, lastC);
+                ans += left+1;
+                lastB = i;
+            }else {
+                int left = Math.min(lastA, lastB);
+                ans += left+1;
+                lastC = i;
+            }
+        }
+        return ans;
+    }
+
+}
