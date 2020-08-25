f2871694ef691b430a4a136f59a6f9d18d535468
徐侠 <xux21@chinaunicom.cn>
2020-08-25 17:32:19 +0800

    2020-08-25

--- algorithm/src/cn/xux/algorithm/leetcode/general/easy/RepeatedSubstringPattern.java
+++ algorithm/src/cn/xux/algorithm/leetcode/general/easy/RepeatedSubstringPattern.java
@@ -1,36 +1,59 @@
 package cn.xux.algorithm.leetcode.general.easy;
 
 /**
- * Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
- * You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
- * Example 1:
- * Input: "abab"
- * Output: True
- * Explanation: It's the substring "ab" twice.
- * Example 2:
- * Input: "aba"
- * Output: False
- * Example 3:
- * Input: "abcabcabcabc"
- * Output: True
- * Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+ * 459. 重复的子字符串
+ * 给定一个非空的字符串，判断它是否可以由它的一个子串重复多次构成。
+ * 给定的字符串只含有小写英文字母，并且长度不超过10000。
+ *
+ * 示例 1:
+ * 输入: "abab"
+ * 输出: True
+ * 解释: 可由子字符串 "ab" 重复两次构成。
+ *
+ * 示例 2:
+ * 输入: "aba"
+ * 输出: False
+ *
+ * 示例 3:
+ * 输入: "abcabcabcabc"
+ * 输出: True
+ * 解释: 可由子字符串 "abc" 重复四次构成。 (或者子字符串 "abcabc" 重复两次构成。)
  */
 public class RepeatedSubstringPattern {
 
+    public static void main(String[] args) {
+        System.out.println(new RepeatedSubstringPattern().repeatedSubstringPattern("aabaaba"));
+    }
+
     public boolean repeatedSubstringPattern(String s) {
+        if(s==null||s.length()<2) {
+            return false;
+        }
         int len = s.length();
         String tmp;
-        for(int i=2;i<=len;i++) {
+        for(int i=1;i<=(int)Math.sqrt(len);i++) {
             if(len%i==0) {
-                tmp = s.substring(0, len/i);
-                int j;
-                for(j=0;j<len;j+=len/i) {
-                    if(!s.substring(j, j+len/i).equals(tmp)) {
+                int j=i;
+                tmp = s.substring(0,j);
+                for(;j<len;j+=i) {
+                    if(!tmp.equals(s.substring(j,j+i))) {
                         break;
                     }
                 }
                 if(j==len) {
                     return true;
+                }
+                if(i!=1) {
+                    j = len/i;
+                    tmp = s.substring(0, j);
+                    for(;j<len;j+=len/i) {
+                        if(!tmp.equals(s.substring(j,j+len/i))) {
+                            break;
+                        }
+                    }
+                    if(j==len) {
+                        return true;
+                    }
                 }
             }
         }
--- algorithm/src/cn/xux/algorithm/leetcode/general/midium/IncreasingSubsequences.java
+++ algorithm/src/cn/xux/algorithm/leetcode/general/midium/IncreasingSubsequences.java
@@ -3,21 +3,50 @@ package cn.xux.algorithm.leetcode.general.midium;
 import java.util.*;
 
 /**
- * Given an integer array,
- * your task is to find all the different possible increasing subsequences of the given array,
- * and the length of an increasing subsequence should be at least 2.
- * Example:
- * Input: [4, 6, 7, 7]
- * Output: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
- * Note:
- * The length of the given array will not exceed 15.
- * The range of integer in the given array is [-100,100].
- * The given array may contain duplicates,
- * and two equal integers should also be considered as a special case of increasing sequence.
+ * 491. 递增子序列
+ * 给定一个整型数组, 你的任务是找到所有该数组的递增子序列，递增子序列的长度至少是2。
+ *
+ * 示例:
+ * 输入: [4, 6, 7, 7]
+ * 输出: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
+ *
+ * 说明:
+ * 给定数组的长度不会超过15。
+ * 数组中的整数范围是 [-100,100]。
+ * 给定数组中可能包含重复数字，相等的数字应该被视为递增的一种情况。
  */
 public class IncreasingSubsequences {
 
+    //迭代
     public List<List<Integer>> findSubsequences(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums==null||nums.length<2) {
+            return res;
+        }
+        Set<List<Integer>> curr = new HashSet<>();
+        curr.add(new ArrayList<>());
+        for(int num : nums) {
+            Set<List<Integer>> next = new HashSet<>();
+            for(List<Integer> pre : curr) {
+                next.add(pre);
+                if(pre.size()==0||num>=pre.get(pre.size()-1)) {
+                    List<Integer> tmp = new ArrayList<>(pre);
+                    tmp.add(num);
+                    next.add(tmp);
+                }
+            }
+            curr = next;
+        }
+        for(List<Integer> list : curr) {
+            if(list.size()>1) {
+                res.add(list);
+            }
+        }
+        return res;
+    }
+
+    //递归
+    public List<List<Integer>> findSubsequences1(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if(nums==null||nums.length<2) {
             return res;
