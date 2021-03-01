0e481784fca976350785fffab8f40951108bd27e
徐侠 <xux21@chinaunicom.cn>
2021-03-01 14:01:50 +0800

    2021-03-01

--- algorithm/src/cn/xux/algorithm/leetcode/general/easy/RangeSumQueryImmutable.java
+++ algorithm/src/cn/xux/algorithm/leetcode/general/easy/RangeSumQueryImmutable.java
@@ -1,18 +1,45 @@
 package cn.xux.algorithm.leetcode.general.easy;
 
+/**
+ * 303. 区域和检索 - 数组不可变
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）
+ * 范围内元素的总和，包含 i、j 两点。
+ * 实现 NumArray 类：
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums
+ * 从索引 i 到 j（i ≤ j）范围内元素的总和，
+ * 包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+ *
+ * 示例：
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
+ *
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ *
+ * 提示：
+ * 0 <= nums.length <= 104
+ * -105 <= nums[i] <= 105
+ * 0 <= i <= j < nums.length
+ * 最多调用 104 次 sumRange 方法
+ */
 public class RangeSumQueryImmutable {
 
     int[] sums;
 
     public RangeSumQueryImmutable(int[] nums) {
-        if(nums==null||nums.length<2) {
-            this.sums = nums;
+        if(nums==null||nums.length==0) {
             return;
         }
-        this.sums = new int[nums.length];
-        this.sums[0] = nums[0];
-        for(int i=1;i<nums.length;i++) {
-            this.sums[i] = this.sums[i-1] + nums[i];
+        this.sums = new int[nums.length+1];
+        for(int i=1;i<=nums.length;i++) {
+            this.sums[i] = this.sums[i-1] + nums[i-1];
         }
     }
 
@@ -20,7 +47,7 @@ public class RangeSumQueryImmutable {
         if(i<0||i>j||this.sums==null||this.sums.length==0) {
             return 0;
         }
-        return i==0?sums[j]:sums[j]-sums[i-1];
+        return sums[j+1]-sums[i];
     }
 
 }
--- algorithm/src/cn/xux/algorithm/leetcode/general/easy/ToeplitzMatrix.java
+++ algorithm/src/cn/xux/algorithm/leetcode/general/easy/ToeplitzMatrix.java
@@ -40,11 +40,9 @@ package cn.xux.algorithm.leetcode.general.easy;
 public class ToeplitzMatrix {
 
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(i>0&&j>0&&matrix[i][j]!=matrix[i-1][j-1]) {
+        for(int i=1;i<matrix.length;i++) {
+            for(int j=1;j<matrix[0].length;j++) {
+                if(matrix[i][j]!=matrix[i-1][j-1]) {
                     return false;
                 }
             }
